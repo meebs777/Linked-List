4c7cad607a16e90c16c465e9dc1069374f2270a5
Mibo <meebsawosika@gmail.com>
2025-11-09 18:51:59 -0700

    feat: add additional methods to linked list

--- linkedlist.js
+++ linkedlist.js
@@ -18,8 +18,85 @@ export function linkedList() {
     const prepend = (value) => {
         if (head === null) append(value);
         else {
-
+            const node = Node(value);
+            node.nextNode = head;
+            head = node;
         }
+    }
+
+    const size = () => {
+        if (head === null) return 0;
+        let count = 0;
+        let curr = head;
+        while (curr.nextNode !== null) {
+            count++;
+            curr = curr.nextNode;
+        }
+        count++;
+        return count;
+    }
+
+    const at = (index) => {
+        if (head === null) return null;
+        if (index > size() - 1) return "Doesn't exist";
+        let curr = head;
+        for (let indx = 0; indx < index; indx++) {
+            curr = curr.nextNode;
+        }
+        return curr;
+    }
+
+    const pop = () => {
+        if (head === null) return;
+        if (head.nextNode === null) {
+            head = null;
+            tail = null;
+            return
+        }
+        let curr = head;
+        let prev = curr;
+        while (curr.nextNode !== null) {
+            prev = curr;
+            curr = curr.nextNode;
+        }
+        prev.nextNode = null;
+        tail = prev;
+    }
+
+    const contains = (value) => {
+        if (head === null) return false;
+        let curr = head;
+        while (curr.nextNode !== null) {
+            if (curr.value === value) {
+                return true;
+            }
+            curr = curr.nextNode;
+        }
+        //Check last node
+        if (curr.value === value) {
+                return true;
+        }
+        return false;
+
+    }
+
+    const find = (value) => {
+        if (head === null) return null;
+        let curr = head;
+        let count = 0;
+        while (curr.nextNode !== null) {
+            
+            if (curr.value === value) {
+                return count;
+            }
+            count++;
+            curr = curr.nextNode;
+        }
+        //Check last node
+        if (curr.value === value) {
+            return count;
+        }
+        return null;
     }
 
     const toString = () => {
@@ -39,7 +116,7 @@ export function linkedList() {
     
     const headNode = () => { return head; }
     const tailNode = () => { return tail; }
-    return{headNode,append,toString,prepend,tailNode}
+    return { headNode, append, toString, prepend, tailNode, size, pop, at, contains, find }
 }
 
 export function Node(value = null,nextNode = null) {
@@ -52,8 +129,13 @@ list.append("two")
 list.append("three")
 list.append("four")
 list.append("five")
+list.prepend("six")
+
 
 console.log(list.toString())
 console.log(list.headNode())
 console.log(list.tailNode())
+console.log(list.size())
+console.log(list.at(6))
+console.log(list.find("five"))
 
